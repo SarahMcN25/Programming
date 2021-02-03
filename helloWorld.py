fd302d62d91b5c6d67f73d72ce82b8be51dd5a07
Sarah McNelis <G00398343@gmit.ie>
2021-02-03 15:06:55 +0000

    test

---
+++ helloWorld.py
@@ -0,0 +1,4 @@
+# This program just prints out hello World
+# Author: Sarah McNelis
+
+print ("Hello World!")
--- week02/hello.py
+++
@@ -1,4 +0,0 @@
-# This program just prints out hello
-# Author: Sarah McNelis
-
-print ("Hello Sarah")
